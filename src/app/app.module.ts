12f42e4e702c6fcfcb782a6ef4212422fcf31df4
anirbmuk <anirbmuk@live.in>
2022-05-07 22:25:07 +0200

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 import { AppComponent } from './app.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, SigninComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatIconModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/signin/signin.component.ts
@@ -0,0 +1,54 @@
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  OnDestroy,
+  ViewChild,
+} from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SigninComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('passwordField') passwordField?: ElementRef<HTMLInputElement>;
+
+  capsLockMessage: string | undefined;
+
+  readonly sigininForm = new FormGroup({
+    emailField: new FormControl('', [Validators.required, Validators.email]),
+    passwordField: new FormControl('', [
+      Validators.required,
+      Validators.min(8),
+    ]),
+  });
+
+  constructor(private readonly cdRef: ChangeDetectorRef) {}
+
+  ngAfterViewInit(): void {
+    const eventHandler = (event: KeyboardEvent | MouseEvent) => {
+      if (event.getModifierState('CapsLock')) {
+        this.capsLockMessage = 'Caps Lock is ON';
+      } else {
+        this.capsLockMessage = undefined;
+      }
+      this.cdRef.markForCheck();
+    };
+
+    this.passwordField?.nativeElement?.addEventListener('keyup', eventHandler);
+    this.passwordField?.nativeElement?.addEventListener(
+      'mousedown',
+      eventHandler
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.passwordField?.nativeElement?.removeAllListeners?.('keyup');
+    this.passwordField?.nativeElement?.removeAllListeners?.('mousedown');
+  }
+}
